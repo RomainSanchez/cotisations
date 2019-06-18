e8c82ec02292680c9ea806bec46cc7e78de695ad
Romain Sanchez <rsanchez@cdg29.bzh>
2019-06-18 11:29:44 +0200

    Check payment label format

--- client/src/app/payments/payments.component.ts
+++ client/src/app/payments/payments.component.ts
@@ -56,4 +56,14 @@ export class PaymentsComponent implements OnInit {
     this.selectedPayment.emit(payment);
   }
 
+  public isLabelValid (label: string) {
+    const regularExpressions = [
+      new RegExp('COTIS_\\d{4}_\\d{2}_d{14}', 'i'),
+      new RegExp('COTIS\\s\\d{4}\\s\\d{2}\\s\\d{14}', 'i')
+    ];
+
+    return (label && label !== ' ' && label !== 'Libellé') &&
+      (label.match(regularExpressions[0]) || label.match(regularExpressions[1]));
+  }
+
 }
