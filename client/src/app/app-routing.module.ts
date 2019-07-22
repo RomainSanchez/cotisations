cc7b0cf90db111980f609e61c9af115e7839cdbc
Romain Sanchez <rsanchez@cdg29.bzh>
2019-07-22 16:38:37 +0200

    Make community list the landing page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,15 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommunitiesComponent } from './communities/communities.component';
 import { MatchComponent } from './match/match.component';
 import { MatchesComponent } from './matches/matches.component';
+import { AccountComponent } from './account/account.component';
+import { CommunityResolver } from './account/community-resolver.service';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/communities', pathMatch: 'full'},
   { path: 'communities', component: CommunitiesComponent },
   { path: 'match', component: MatchComponent },
-  { path: 'matches', component: MatchesComponent }
+  { path: 'matches', component: MatchesComponent },
+  { path: 'account/:communityId',
+    component: AccountComponent,
+    resolve: {community: CommunityResolver}
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [CommunityResolver]
 })
 export class AppRoutingModule { }
